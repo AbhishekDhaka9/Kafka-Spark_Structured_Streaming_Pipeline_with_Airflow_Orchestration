ad2c9c077ac32ccf1615efa39d1b3a7bd1f766b7
Abhishek <Abhishekdhakaece2001@gmail.com>
2026-05-21 22:49:12 +0530

    2nd

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import pandas as pd
-from sklearn.model_selection import train_test_split
